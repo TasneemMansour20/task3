9417a342591b845bee52dceda9709b09bff49d51
Tasneem <tota47nabil@gmail.com>
2023-05-26 13:28:44 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -3,92 +3,129 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
+  runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey[200],
         appBar: AppBar(
-          title: Text('Rock Scissors Paper game'),
+          title: Text('Rock Scissors Paper'),
           backgroundColor: Colors.pink,
         ),
-        body: dicePage(),
+        body: DicePage(),
       ),
     ),
   );
 }
 
-class dicePage extends StatefulWidget {
+class DicePage extends StatefulWidget {
   @override
-  State<dicePage> createState() => _dicePageState();
+  State<DicePage> createState() => _DicePageState();
 }
 
-class _dicePageState extends State<dicePage> {
-  int lefbtn = Random().nextInt(3);
-  int rightbtn = Random().nextInt(3);
+class _DicePageState extends State<DicePage> {
+  int leftBtn = Random().nextInt(3);
+  int rightBtn = Random().nextInt(3);
+  String gameResult = '';
+
+  void playGame() {
+    int mySelection = leftBtn;
+    int systemSelection = rightBtn;
+
+    if ((mySelection == 0 && systemSelection == 2) ||
+        (mySelection == 1 && systemSelection == 0) ||
+        (mySelection == 2 && systemSelection == 1)) {
+      setState(() {
+        gameResult = 'You win!';
+      });
+    } else if ((systemSelection == 0 && mySelection == 2) ||
+        (systemSelection == 1 && mySelection == 0) ||
+        (systemSelection == 2 && mySelection == 1)) {
+      setState(() {
+        gameResult = 'System wins!';
+      });
+    } else {
+      setState(() {
+        gameResult = 'No one wins';
+      });
+    }
+  }
 
   void randMe() {
     setState(() {
-      lefbtn = Random().nextInt(3);
-      rightbtn = Random().nextInt(3);
+      leftBtn = Random().nextInt(3);
+      rightBtn = Random().nextInt(3);
+      gameResult = '';
+      playGame();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 60),
-      child: Row(
+      padding: const EdgeInsets.only(top: 8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(
-            child: TextButton(
-              child: Column(
-                children: [
-                  Image.asset("images/btn$lefbtn.png"),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(
-                    "Me",
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                  ),
-                ],
-              ),
-              onPressed: randMe,
-            ),
-          ),
-          Column(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              Expanded(
+                child: TextButton(
+                  child: Column(
+                    children: [
+                      Image.asset("images/btn$leftBtn.png"),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "Me",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[900]),
+                      ),
+                    ],
+                  ),
+                  onPressed: randMe,
+                ),
+              ),
               Text(
                 "VS",
                 style: TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
+                  color: Colors.blue[900],
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  child: Column(
+                    children: [
+                      Image.asset("images/btn$rightBtn.png"),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        "System",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[900]),
+                      ),
+                    ],
+                  ),
+                  onPressed: randMe,
                 ),
               ),
             ],
           ),
-          TextButton(
-            child: Expanded(
-              child: Column(
-                children: [
-                  Image.asset("images/btn$rightbtn.png"),
-                  SizedBox(
-                    height: 12,
-                  ),
-                  Text(
-                    "Phone",
-                    style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black),
-                  ),
-                ],
-              ),
+          SizedBox(height: 20),
+          Text(
+            gameResult,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.pink,
             ),
-            onPressed: randMe,
           ),
         ],
       ),
